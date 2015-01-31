ea80b61617a2938ad0db121337cf0309469e78a0
el aras <openmason@gmail.com>
2015-01-31 20:34:10 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+[![build status](https://secure.travis-ci.org/openmason/wspipe.png)](http://travis-ci.org/openmason/wspipe)
+# Title
+description
+
+# Features
+list of features
+
+# Usage
+How to use
+
+# Tests
+How to run tests
+    
---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * wspipe - main file
+ * copyright (c) 2015 openmason.
+ * MIT Licensed.
+ */
+
+// JSON RPC library entry point
+var WebSocketPipe = function(module, debug) {
+  this.debug = debug || false;
+};
+
+module.exports = WebSocketPipe;
+
+// -- EOF
